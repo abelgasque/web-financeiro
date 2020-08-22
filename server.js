e1903676308e5184969bc0c2b83446a8ae004ce6
abelgasque <55210807+abelgasque@users.noreply.github.com>
2020-08-22 20:44:07 -0300

    deploy

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const nomeApp = process.env.npm_package_name;
 const app = express();
-
+const cors = require('cors');
+app.use(cors());
 app.use(express.static(`${__dirname}/dist/Financeiro-web`));
 
 app.get('/*', (req, res) => {
@@ -14,5 +15,5 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
-  
+
 app.listen(process.env.PORT || 4200);
