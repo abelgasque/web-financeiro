37f717b245a8bc8ba57093e211f4dca447e454b4
abelgasque <55210807+abelgasque@users.noreply.github.com>
2020-08-22 20:42:49 -0300

    deploy

--- server.js
+++ server.js
@@ -9,4 +9,10 @@ app.get('/*', (req, res) => {
 res.sendFile(path.join(`${__dirname}/dist/Financeiro-web/index.html`));
 });
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
 app.listen(process.env.PORT || 4200);
