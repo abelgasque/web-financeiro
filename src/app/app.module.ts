fc717cf79036df66fcaafcbfb9fdb0ba5e49073d
abelgasque <55210807+abelgasque@users.noreply.github.com>
2020-08-22 22:46:25 -0300

    correção deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { JwtModule } from "@auth0/angular-jwt";
+
+import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { CoreModule } from './core/core.module';
 import { SegurancaModule } from './seguranca/seguranca.module';
+
+export function tokenGetter() {
+  return localStorage.getItem("access_token");
+}
 
 @NgModule({
   declarations: [
@@ -17,7 +25,15 @@ import { SegurancaModule } from './seguranca/seguranca.module';
     AppRoutingModule,
    
     CoreModule,
-    SegurancaModule
+    SegurancaModule,
+    HttpClientModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        allowedDomains: environment.tokenWhitelistedDomains,
+        disallowedRoutes: environment.tokenBlacklistedRoutes,
+      },
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/seguranca/seguranca.module.ts
+++ src/app/seguranca/seguranca.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { AuthGuard } from './auth.guard'
-import { JwtModule } from "@auth0/angular-jwt";
 
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { MatButtonModule } from '@angular/material/button';
@@ -13,17 +11,12 @@ import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
 import { ButtonModule } from 'primeng/button';
 
-import { environment } from 'src/environments/environment';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { SegurancaComponent } from './seguranca.component';
 import { SharedModule } from '../shared/shared.module';
 import { LogoutService } from './logout.service';
 import { UsuarioCadastroComponent } from './usuario-cadastro/usuario-cadastro.component';
 import { SegurancaRoutingModule } from './seguranca.routing.module';
-
-export function tokenGetter() {
-  return localStorage.getItem("access_token");
-}
 
 @NgModule({
   declarations: [
@@ -35,14 +28,6 @@ export function tokenGetter() {
     CommonModule,
     RouterModule,
     FormsModule,
-    HttpClientModule,
-    JwtModule.forRoot({
-      config: {
-        tokenGetter: tokenGetter,
-        allowedDomains: environment.tokenWhitelistedDomains,
-        disallowedRoutes: environment.tokenBlacklistedRoutes,
-      },
-    }),
 
     FlexLayoutModule,
 
