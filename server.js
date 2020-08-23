da671fd692ee6ee43933bbaf5a5d7a8eab51c813
abelgasque <55210807+abelgasque@users.noreply.github.com>
2020-08-22 21:31:24 -0300

    correção deploy

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const path = require('path');
-const nomeApp = process.env.npm_package_name;
+const cors = require('cors');
 const app = express();
-app.use(express.static(`${__dirname}/dist/Financeiro-web`));
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors());
+
+app.use(express.static(`${__dirname}/dist/Financeiro-web`));
 
 app.get('/*', (req, res) => {
 res.sendFile(path.join(`${__dirname}/dist/Financeiro-web/index.html`));
